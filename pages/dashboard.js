4da01c63f7c788d7b0665e6b03bbcc2e99ba3b2c
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-01-10 15:41:29 -0500

    Dynamic queries and map popups

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,6 +2,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
+import { renderToString } from 'react-dom/server';
 
 export default function Dashboard(props) {
     const { MAPBOXGL_ACCESS_TOKEN, datasets, baseUrl } = props;
@@ -16,11 +17,28 @@ export default function Dashboard(props) {
             center: [-74.5, 40], // starting position [lng, lat]
             zoom: 9, // starting zoom
         });
+
         map.current.on('load', () => {
-            const { slug, layerUrl } = datasets[0];
+            const { slug, layerUrl } = datasets[1];
             addRasterTileLayerToMap(map.current, slug, layerUrl);
-            const { source } = datasets[1];
+            const { source } = datasets[0];
             addVectorTileLayerToMap(map.current, 'wdpa-tile-cache', source, 'wdpa_protected_areas');
+        });
+
+        map.current.on('click', 'wdpa-tile-cache', async (e) => {
+            const features = map.current.queryRenderedFeatures(e.point, {layers: ['wdpa-tile-cache']});
+            new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(await popupContentForWDPA(features[0]))
+            .addTo(map.current);
+        });
+
+        map.current.on('mouseenter', 'wdpa-tile-cache', () => {
+            map.current.getCanvas().style.cursor = 'pointer';
+        });
+
+        map.current.on('mouseleave', 'wdpa-tile-cache', () => {
+            map.current.getCanvas().style.cursor = '';
         });
     }, []);
 
@@ -76,6 +94,54 @@ export default function Dashboard(props) {
             map.current.setLayoutProperty(layers[i], 'visibility', 'visible');
     }
 
+    const popupContentForWDPA =  async (f) => {
+        const landAreaHectare = (f.properties['rep_area'] - f.properties['rep_m_area']) * 100;
+        const wdpaId = f.properties['wdpaid'];
+        const tclResponse = await callApiQueryWDPATreeCoverLoss(wdpaId, 2019, 30);
+
+        return renderToString(
+            <div>
+                <span>name:{' '}
+                    <a target='_blank' href={`https://protectedplanet.net/${wdpaId}`}>{f.properties['name']}</a>
+                </span>
+                <br />
+                <span>
+                    wdpaid: {wdpaId}
+                </span>
+                <br />
+                <span>
+                    land area (ha): {landAreaHectare.toFixed(2)}
+                </span>
+                <br />
+                {tclResponse.keys &&
+                    <span>
+                        2019 TCL area (ha): {tclResponse['data'][0]['tcl_ha'].toFixed(2)}
+                    </span>
+                }
+            </div>
+        );
+    }
+
+    const callApiQueryWDPATreeCoverLoss = async (wdpaId, year, threshold) => {
+        const queryDatasetId = 'a4d92f66-83f4-40f9-9d70-17297ef90e63'; // this ID (from tutorial) appears to no longer exist
+        const sqlQuery = 'https://api.resourcewatch.org/v1/query/' +
+            '?sql=SELECT SUM(umd_tree_cover_loss__ha) AS tcl_ha ' +
+            'FROM ' + queryDatasetId +
+            ' WHERE wdpa_protected_area__id=' + wdpaId +
+            ' AND umd_tree_cover_loss__year=' + year +
+            ' AND umd_tree_cover_density__threshold=' + threshold;
+        const data = await axios.get(sqlQuery)
+            .then(({ data }) => {
+                return data;
+            })
+            .catch((err) => {
+                console.log(err);
+                return {};
+            });
+        console.log(data);
+        return data;
+    }
+
     return (
         <div className='container'>
             <div id='map'></div>
@@ -103,41 +169,41 @@ export async function getServerSideProps(context) {
         baseUrl: process.env.BASEURL,
     }
 
-    const resps = await Promise.allSettled(
-        uuids.map((u) => {
-            return fetchDataset(u);
-        }));
-
-    const datasets = resps.map(d => d.value);
+    const datasets = await fetchDatasets(uuids);
 
     return { props: { ...config, datasets } };
 }
 
-const fetchDataset = async (uuid) => {
-    return axios.get(`https://api.resourcewatch.org/v1/dataset/${uuid}/?includes=layer,metadata`)
-        .then(({ data }) => {
-            const type = getLayerType(data);
-            if (type === 'raster')
-                return {
-                    data: data,
-                    type: getLayerType(data),
-                    layerUrl: getTileLayerUrl(data),
-                    slug: getLayerSlug(data),
-                }
-            else
-                return {
-                    data: data,
-                    type: getLayerType(data),
-                    source: getTileLayerSourceForWDPAGeometries(data),
-                }
+const fetchDatasets = async (uuids) => {
+    const datasets = await axios.post('https://api.resourcewatch.org/v1/dataset/find-by-ids?includes=layer,metadata', { ids: uuids })
+        .then(({ data: { data } }) => {
+            return data.map((data) => {
+                const type = getLayerType(data);
+                if (type === 'raster')
+                    return {
+                        data: data,
+                        type: getLayerType(data),
+                        layerUrl: getTileLayerUrl(data),
+                        slug: getLayerSlug(data),
+                    }
+                else
+                    return {
+                        data: data,
+                        type: getLayerType(data),
+                        source: getTileLayerSourceForWDPAGeometries(data),
+                    }
+            });
         })
         .catch(err => {
-            console.log(err);
+            console.error(err);
+            return [];
         });
+    
+    return datasets;
 }
 
 const getTileLayerUrl = (obj) => {
-    const layerConfig = obj['data']['attributes']['layer'][0]['attributes']['layerConfig'];
+    const layerConfig = obj['attributes']['layer'][0]['attributes']['layerConfig'];
     const defaultParams = layerConfig['params_config'];
 
     let url = layerConfig['source']['tiles'][0];
@@ -151,15 +217,15 @@ const getTileLayerUrl = (obj) => {
 }
 
 const getLayerSlug = (obj) => {
-    return obj['data']['attributes']['layer'][0]['attributes']['slug'];
+    return obj['attributes']['layer'][0]['attributes']['slug'];
 }
 
 const getLayerType = (obj) => {
-    const layerConfig = obj['data']['attributes']['layer'][0]['attributes']['layerConfig'];
+    const layerConfig = obj['attributes']['layer'][0]['attributes']['layerConfig'];
     return layerConfig['source']['type'];
 }
 
 const getTileLayerSourceForWDPAGeometries = (obj) => {
     // drill down to get a useful object
-    return obj['data']['attributes']['layer'][0]['attributes']['layerConfig']['source'];
+    return obj['attributes']['layer'][0]['attributes']['layerConfig']['source'];
 }
