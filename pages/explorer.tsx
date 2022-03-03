c5e820a22302a6b896bb16f6679e303f439f366e
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-03-03 12:40:57 -0500

    Revise structure to explorer page

--- pages/explorer.tsx
+++ pages/explorer.tsx
@@ -1,11 +1,11 @@
-import LayoutExplorer from "layout/layout/explorer";
+import LayoutExplorer from "layout/explorer";
 import { GetStaticProps } from "next";
 
 interface ExplorerProps {
     MAPBOXGL_ACCESS_TOKEN: string
 }
 
-export default function Explorer({ MAPBOXGL_ACCESS_TOKEN }: ExplorerProps) {
+const Explorer = ({ MAPBOXGL_ACCESS_TOKEN }: ExplorerProps) => {
     return (
         <LayoutExplorer MAPBOXGL_ACCESS_TOKEN={MAPBOXGL_ACCESS_TOKEN} />
     )
@@ -15,3 +15,5 @@ export const getStaticProps: GetStaticProps = () => {
     const MAPBOXGL_ACCESS_TOKEN = process.env.MAPBOXGL_ACCESS_TOKEN;
     return { props: { MAPBOXGL_ACCESS_TOKEN: MAPBOXGL_ACCESS_TOKEN } }
 }
+
+export default Explorer;
