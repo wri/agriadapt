1f242ea6fa1b4d307d30a75f6fb82d7ced418eb1
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-03-22 17:57:58 -0400

    Add back loading state effects

--- layout/layout/layout-app/component.jsx
+++ layout/layout/layout-app/component.jsx
@@ -45,12 +45,12 @@ class LayoutApp extends Component {
     } = this.props;
 
     router.events.on('routeChangeStart', () => {
-      // updateIsLoading(true);
+      updateIsLoading(true);
       if (Progress && Progress.Component.instance) Progress.show();
     });
 
     router.events.on('routeChangeComplete', () => {
-      // updateIsLoading(false);
+      updateIsLoading(false);
       if (Progress && Progress.Component.instance) Progress.hideAll();
     });
 
@@ -123,7 +123,7 @@ class LayoutApp extends Component {
 
         <Header pageHeader={pageHeader} />
 
-        {/* <Progress.Component /> */}
+        <Progress.Component />
 
         {children}
 
