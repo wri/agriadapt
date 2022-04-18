63aa06818bbe991b8cab308160231d82d3015a6c
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-04-18 14:17:05 -0400

    Add more datasets and adjust layer details sidebar width

--- constants/app.js
+++ constants/app.js
@@ -1,20 +1,19 @@
 export const TRANSIFEX_BLACKLIST = [
-  "/app/embed/EmbedDashboard",
-  "/app/embed/EmbedMap",
-  "/app/embed/EmbedTable",
-  "/app/embed/EmbedText",
-  "/app/embed/EmbedWidget",
-  "/app/embed/EmbedEmbed",
-  "/app/embed/EmbedDataset",
-  "/app/embed/EmbedSimilarDatasets",
-  "/app/explore/embed",
+  '/app/embed/EmbedDashboard',
+  '/app/embed/EmbedMap',
+  '/app/embed/EmbedTable',
+  '/app/embed/EmbedText',
+  '/app/embed/EmbedWidget',
+  '/app/embed/EmbedEmbed',
+  '/app/embed/EmbedDataset',
+  '/app/embed/EmbedSimilarDatasets',
+  '/app/explore/embed',
 ];
 
 export const FULLSCREEN_PAGES = [
-  "/data/explore",
-  "/data/pulse",
-  "/sign-in",
-  "/explore",
+  '/data/pulse',
+  '/sign-in',
+  '/explore',
 ];
 
 export const EXPLORE_DATASETS_IDS = [
@@ -26,7 +25,10 @@ export const EXPLORE_DATASETS_IDS = [
   'faf79d2c-5e54-4591-9d70-4bd1029c18e6', // Projected Change in Cumulative Precipitation
   'c66d7f3a-d1a8-488f-af8b-302b0f2c3840', // Water Stress
   'f4a83ced-4aea-4cf5-aa71-00927fe707e1', // Seasonal Variability of Water
+  'cit016-Road-Network_replacement', // Global Roads Open Access Data Set, v1 (gROADSv1)
+  '3d8e2e82-b33a-4898-90e5-6e4a1d007b82', // Projected Change in Annual Average Minimum Temperature
+  '641c0a35-f2e5-4198-8ed9-576ea7e9685a', // 2018 Nominal Rate of Protection (%)
+  '4d2d47c1-fed1-4484-83e3-c91c3f6f7315', // Vulnerability to Climate Change Index
 ];
 
-
-export const HOTJAR_ROUTES = ["/data/explore"];
+export const HOTJAR_ROUTES = ['/data/explore'];
