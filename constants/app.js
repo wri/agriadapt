bf25ba4c910f6ce77dc22a56167a337a075f5e20
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-06-16 16:23:15 -0400

    Fix dataset ID typo

--- constants/app.js
+++ constants/app.js
@@ -32,7 +32,7 @@ export const EXPLORE_DATASETS_IDS = [
   'ea2db3a6-49c8-4d41-a2ab-758eb6fe4bc0', // Landslide Susceptibility
   'c4b12251-2d61-458f-a2c0-096c37901ade', // Projected Change in Annual Average Maximum Temeprature
   '4ca6826c-718d-457d-b4e2-e9277d7ed62c', // Projected Change in Annual Average Temperature
-  'cit01701-Travel-Time-to-Major-Cities', // Accessibility to Cities
+  'ccbcaf7b-1619-4298-8275-b135d1e8e04e', // Accessibility to Cities
   'faf79d2c-5e54-4591-9d70-4bd1029c18e6', // Projected Change in Cumulative Precipitation
   'c66d7f3a-d1a8-488f-af8b-302b0f2c3840', // Water Stress
   'f4a83ced-4aea-4cf5-aa71-00927fe707e1', // Seasonal Variability of Water
