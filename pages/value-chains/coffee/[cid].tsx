1930accd5a16a655e5983bf12d3cb8b1ed2b3415
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-07-07 10:58:42 -0400

    Temporarily storing coffee layers in page component
    Co-authored-by: Vincent Zhu <vincentlinzhu@users.noreply.github.com>

--- pages/value-chains/coffee/[cid].tsx
+++ pages/value-chains/coffee/[cid].tsx
@@ -1,9 +1,23 @@
-import LayoutCoffee from "layout/value-chains/coffee";
-import { GetStaticPaths, GetStaticProps } from "next";
-import { ValueChainPageProps } from "types/value-chain";
+import LayoutCoffee from 'layout/value-chains/coffee';
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next';
+import { ValueChainPageProps } from 'types/value-chain';
 
 const CoffeeCountryPage = (props: ValueChainPageProps) => {
   return <LayoutCoffee {...props} />;
+};
+
+const coffeeLayers = [
+  'foo_005_rw1_crop_area_production_spam2010V2r0_global_H_ACOF_A',
+  'foo_005_rw1_crop_area_production_spam2010V2r0_global_H_RCOF_A',
+  'foo_005_rw1_crop_area_production_spam2010V2r0_global_P_ACOF_A',
+  'foo_005_rw1_crop_area_production_spam2010V2r0_global_P_RCOF_A',
+  'foo_005_rw1_crop_area_production_spam2010V2r0_global_Y_ACOF_A',
+  'foo_005_rw1_crop_area_production_spam2010V2r0_global_Y_RCOF_A'
+]
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  
+  return { props: {} };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
