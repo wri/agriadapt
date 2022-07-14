49024c4ab9293e6b99208e78a7686cbdf62fe410
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-07-14 02:15:28 -0700

    Fix typescript error

--- layout/value-chains/content/item-intro/index.ts
+++
@@ -1,7 +0,0 @@
-import { RootState } from "lib/store";
-import { connect } from "react-redux";
-import ItemIntro from "./component";
-
-export default connect((state: RootState) => ({
-    activeItem: state.value_chains.activeItem
-}))(ItemIntro);
