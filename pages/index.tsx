16129b02b943f7b06b879d982a1e217a1cea3d4e
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-08-05 14:13:04 -0400

    IP address testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import LayoutHome from 'layout/app/home';
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export default function Home() {
+export default function Home({ req }) {
+  console.log(req);
   return <LayoutHome />;
 }
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, locale }) => {
   return {
-    props: { ...(await serverSideTranslations(locale, ['landing', 'common', 'header', 'footer'])) },
+    props: { req, ...(await serverSideTranslations(locale, ['landing', 'common', 'header', 'footer'])) },
   };
 };
