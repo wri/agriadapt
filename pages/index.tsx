4f4676e4afd9cd9f845d8dda46121802c95992f0
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-08-05 15:26:33 -0400

    Cloudfront viewer country testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,16 @@ import LayoutHome from 'layout/app/home';
 import { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export default function Home({ req }) {
-  console.log(req);
+export default function Home({ iso2 }) {
+  console.log('viewer country', iso2);
   return <LayoutHome />;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, locale }) => {
+  // Get viewer country ISO2 code from Amazon Cloudfront Header
+  const iso2 = req.headers['cloudfront-viewer-country'];
+  // India = 'IN'
   return {
-    props: { req, ...(await serverSideTranslations(locale, ['landing', 'common', 'header', 'footer'])) },
+    props: { iso2, ...(await serverSideTranslations(locale, ['landing', 'common', 'header', 'footer'])) },
   };
 };
