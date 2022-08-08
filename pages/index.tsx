15a6553585dc87cf4020f3740d75ce8eb92add4a
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-08-08 01:28:12 -0400

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import LayoutHome from 'layout/app/home';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export default function Home({ iso2 }) {
-  console.log('viewer country', iso2);
+export default function Home() {
   return <LayoutHome />;
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, locale }) => {
-  // Get viewer country ISO2 code from Amazon Cloudfront Header
-  const iso2 = req.headers['cloudfront-viewer-country'];
-  // India = 'IN'
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
-    props: { iso2, ...(await serverSideTranslations(locale, ['landing', 'common', 'header', 'footer'])) },
+    props: { ...(await serverSideTranslations(locale, ['landing', 'common', 'header', 'footer'])) },
   };
 };
