3d94654f7eb517f7941090a5c44641cd80d81567
Vincent Zhu <vincent.zhu@locana.co>
2022-08-14 14:14:38 -0700

    Landing Page Map Carousel Images Done

--- layout/app/home/home-analysis/analysis-card/component.tsx
+++ layout/app/home/home-analysis/analysis-card/component.tsx
@@ -1,10 +1,11 @@
 import loader from 'lib/imageLoader';
 import Image from 'next/image';
 import mapImage from 'public/images/components/layout/landing/analysis-map.png';
+import mapImage1 from 'public/images/components/layout/landing/Home_Img_Cotton.png';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 
-const AnalysisCard = ({ title, button, image_alt }) => {
+const AnalysisCard = ({ title, button, image, image_alt }) => {
   const { t } = useTranslation('landing');
   return (
     <div className="c-analysis-card">
@@ -13,7 +14,7 @@ const AnalysisCard = ({ title, button, image_alt }) => {
         <a className="c-button -secondary relative z-10">{t(button.label)}</a>
       </Link>
       <div className="c-analysis-card-image">
-        <Image loader={loader} unoptimized src={mapImage} alt={t(image_alt)} />
+        <Image loader={loader} unoptimized src={image} alt={t(image_alt)}/>
       </div>
     </div>
   );
