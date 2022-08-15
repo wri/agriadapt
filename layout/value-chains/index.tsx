867c2dfcb7c7aa142000322aa07e8d2ee29b5c9c
Vincent Zhu <vincent.zhu@locana.co>
2022-08-15 15:04:44 -0700

    Started Dynamic Translate Helper Function
    Co-authored-by: Kyle Aig-Imoukhuede <kyleaig2@users.noreply.github.com>

--- layout/value-chains/index.tsx
+++ layout/value-chains/index.tsx
@@ -9,6 +9,8 @@ import { capitalizeFirstLetter } from 'utils/utils';
 import { connect } from 'react-redux';
 import { RootState } from 'lib/store';
 import { StaticImageData } from 'next/image';
+import useDynamicTranslate from 'hooks/translate';
+import {useEffect} from 'react';
 
 interface LayoutCropProps {
   header: any;
@@ -40,11 +42,15 @@ const LayoutCrop = ({
   crop,
   quotes,
 }: LayoutCropProps) => {
+
+  const { dt } = useDynamicTranslate();
+
   return (
     // TODO: Translate
     <Layout title={`${capitalizeFirstLetter(crop)} (${country.label})`}>
       <div className="l-crop">
         <IntroHeader crop={crop} {...header} countries={countries} />
+        {dt('hey')}
         <div className="l-container">
           <div className="row">
             <div className="column small-12">
