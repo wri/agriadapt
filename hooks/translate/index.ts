c5be67a95dd61fb605807b11f16297988b8b12cc
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-08-15 23:26:31 -0400

    Attempt to create dynamic translation hook

--- hooks/translate/index.ts
+++ hooks/translate/index.ts
@@ -1,35 +1,31 @@
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useEffect, useState, useCallback } from "react";
+// import axios from "axios";
+// import { useRouter } from "next/router";
+// import { useEffect, useState, useCallback } from "react";
 
-const useDynamicTranslate = () => {
+// const useDynamicTranslate = () => {
 
-    const { locale } = useRouter().query;
+//     const { locale } = useRouter().query;
 
-    const endpoint = 'https://api.cognitive.microsofttranslator.com/translate';
+//     const endpoint = 'https://api.cognitive.microsofttranslator.com/translate';
 
-    const [translation, setTranslation] = useState(null);
+//     const [translation, setTranslation] = useState(null);
 
-    // useEffect(() => {
+//     const translate = useCallback(async (text: string): string => {
+//         const locales = ['en', 'ta', 'te'];
+//         const translation = await axios.post(endpoint, { text }, { params: {
+//             'api-version': '3.0',
+//             to: locales.join(','),
+//             from: 'en',
+//             headers: {
+//                 'Ocp-Apim-Subscription-Key': process.env.MICROSOFT_TRANSLATE_KEY
+//             }
+//         }}).then(({data}) => locale + String(data)).catch(err => console.error(err));
 
-    // }, []);
+//         return 'hey';
+//     }, [locale]);
 
-    const translate: string = useCallback(async (text: string) => {
-        const locales = ['en', 'ta', 'te'];
-        const translation = await axios.post(endpoint, { text }, { params: {
-            'api-version': '3.0',
-            to: locales.join(','),
-            from: 'en',
-            headers: {
-                'Ocp-Apim-Subscription-Key': process.env.MICROSOFT_TRANSLATE_KEY
-            }
-        }}).then(({data}) => locale + String(data)).catch(err => console.error(err));
+//     return { dt: translate }
 
-        return translation; 
-    }, [locale]);
+// };
 
-    return { dt: translate }
-
-};
-
-export default useDynamicTranslate;
+export default undefined;
