d3c09566e38054f3a15528d7a200c819bea4a915
Kyle Aig-Imoukhuede <kyle.aig@critigen.com>
2022-08-16 21:35:50 -0400

    Product lunch

--- next.config.js
+++ next.config.js
@@ -11,11 +11,11 @@ module.exports = {
 
   async redirects() {
     return [
-      {
-        source: '/((?!maintenance)(?!_next)(?!static).*)',
-        destination: '/maintenance',
-        permanent: false,
-      },
+      // {
+      //   source: '/((?!maintenance)(?!_next)(?!static).*)',
+      //   destination: '/maintenance',
+      //   permanent: false,
+      // },
       {
         source: '/crops',
         destination: '/crops/coffee',
