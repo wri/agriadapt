ba0b9aec6a567ccbac4c2b7be60d9b6aaddbc887
Tim Anderegg <timothy.anderegg@gmail.com>
2026-09-04 15:04:37 -0400

    Adds deprecation banner to the app. (#18)

--- README.md
+++ README.md
@@ -2,7 +2,12 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+Install packages:
+```bash
+npm install --legacy-peer-deps # Needed for @widget-editor
+```
+
+Run the development server:
 
 ```bash
 npm run dev
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,18 @@ import { wrapper } from 'lib/store';
 import MediaContextProvider from "lib/media";
 // import MaintenancePage from './maintenance';
 
+import DeprecationBanner from 'components/deprecation-banner/DeprecationBanner';
+
 const queryClient = new QueryClient();
 
 function AgriAdaptApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <MediaContextProvider>
+        <DeprecationBanner>
+          Thank you for visting Agriadapt. This site is no longer being updated and will be archived in the coming months.
+          <br/>See <a href="https://www.wri.org/data/data-applications/" target="_blank">WRI's Applications Portfolio</a> or visit the <a href="https://datasets.wri.org/" target="_blank">Data Explorer</a> to browse WRI data.
+        </DeprecationBanner>
         <Component {...pageProps} />
         {/* <MaintenancePage /> */}
       </MediaContextProvider>
